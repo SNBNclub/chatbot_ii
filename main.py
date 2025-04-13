d380bc6db89d708cc957b853280128f14b06351f
Egor Albov <albov.egor@gmail.com>
2025-04-13 11:24:37 +0300

    add psql database using docker & clean repo

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import asyncio
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from aiogram import Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from confige import BotConfig
+from handlers import errors, user
+from instance import bot
+from database.models import async_main
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def register_routers(dp: Dispatcher) -> None:
+    dp.include_routers(errors.router, user.router)
 
 
-# Press the green button in the gutter to run the script.
+async def main() -> None:
+    await async_main()
+
+    config = BotConfig(
+        admin_ids=[],
+        welcome_message=""
+    )
+    dp = Dispatcher(storage=MemoryStorage())
+    dp["config"] = config
+
+    register_routers(dp)
+
+    try:
+        await dp.start_polling(bot, skip_updates=True)
+    except Exception as _ex:
+        print(f'Exception: {_ex}')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    asyncio.run(main())
